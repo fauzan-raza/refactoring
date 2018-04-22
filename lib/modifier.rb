56b6d11c340fceddb3474a987b2015bf17dad37f
Fauzan Raza <fauzan.raza@keeptruckin.com>
2018-04-23 03:53:21 +0500

    cleanup in methods

--- lib/modifier.rb
+++ lib/modifier.rb
@@ -1,18 +1,22 @@
 require File.expand_path('lib/combiner',File.dirname(__FILE__))
+require File.expand_path('lib/helper',File.dirname(__FILE__))
 require 'csv'
 require 'date'
 
 class Modifier
 
-	KEYWORD_UNIQUE_ID = 'Keyword Unique ID'
-	LAST_VALUE_WINS = ['Account ID', 'Account Name', 'Campaign', 'Ad Group', 'Keyword', 'Keyword Type', 'Subid', 'Paused', 'Max CPC', 'Keyword Unique ID', 'ACCOUNT', 'CAMPAIGN', 'BRAND', 'BRAND+CATEGORY', 'ADGROUP', 'KEYWORD']
-	LAST_REAL_VALUE_WINS = ['Last Avg CPC', 'Last Avg Pos']
-	INT_VALUES = ['Clicks', 'Impressions', 'ACCOUNT - Clicks', 'CAMPAIGN - Clicks', 'BRAND - Clicks', 'BRAND+CATEGORY - Clicks', 'ADGROUP - Clicks', 'KEYWORD - Clicks']
-	FLOAT_VALUES = ['Avg CPC', 'CTR', 'Est EPC', 'newBid', 'Costs', 'Avg Pos']
+	KEYWORD_UNIQUE_ID 		= 'Keyword Unique ID'
+	LAST_VALUE_WINS 			= ['Account ID', 'Account Name', 'Campaign', 'Ad Group', 'Keyword', 'Keyword Type', 'Subid', 'Paused', 'Max CPC', 'Keyword Unique ID', 'ACCOUNT', 'CAMPAIGN', 'BRAND', 'BRAND+CATEGORY', 'ADGROUP', 'KEYWORD']
+	LAST_REAL_VALUE_WINS 	= ['Last Avg CPC', 'Last Avg Pos']
+	INT_VALUES						= ['Clicks', 'Impressions', 'ACCOUNT - Clicks', 'CAMPAIGN - Clicks', 'BRAND - Clicks', 'BRAND+CATEGORY - Clicks', 'ADGROUP - Clicks', 'KEYWORD - Clicks']
+	FLOAT_VALUES 					= ['Avg CPC', 'CTR', 'Est EPC', 'newBid', 'Costs', 'Avg Pos']
+	COMMISSION_VALUES 		= ['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value']						
+	NUM_OF_COMMISSIONS		= ['number of commissions']
 
-  LINES_PER_FILE = 120000
+  LINES_PER_FILE 				= 120000
 
-  DEFAULT_CSV_OPTIONS = { col_sep: "\t", headers: :first_row }
+  READ_CSV_OPTIONS 		  = { col_sep: "\t", headers: :first_row }
+  WRITE_CSV_OPTIONS 		= { col_sep: "\t", headers: :first_row, row_sep: "\r\n" }
 
 	def initialize(saleamount_factor, cancellation_factor)
 		@saleamount_factor = saleamount_factor
@@ -21,7 +25,6 @@ class Modifier
 
 	def modify(output, input)
 		input = sort(input)
-
 		input_enumerator = lazy_read(input)
 
 		combiner = Combiner.new do |value|
@@ -40,31 +43,52 @@ class Modifier
 			end
 		end
 
-    done = false
+    create_files(output, merger)
+
+	end
+
+	def create_files(output, merger)
+		done 			 = false
     file_index = 0
-    file_name = output.gsub('.txt', '')
+    file_name  = output.gsub('.txt', '')
+    
     while not done do
-		  CSV.open(file_name + "_#{file_index}.txt", "wb", { col_sep: "\t", headers: :first_row, row_sep: "\r\n" }) do |csv|
+		  CSV.open(file_name + "_#{file_index}.txt", "wb", WRITE_CSV_OPTIONS) do |csv|
 			  headers_written = false
-        line_count = 0
-			  while line_count < LINES_PER_FILE
-				  begin
+        line_count 		  = 0
+			  
+			  unless headers_written
+			  	merged = merger.next
+				  csv << merged.keys
+				  csv << merged
+				  headers_written = true
+          line_count +=2
+				end
+				 
+				begin
+			  	while line_count < LINES_PER_FILE
 					  merged = merger.next
-					  if not headers_written
-						  csv << merged.keys
-						  headers_written = true
-              line_count +=1
-					  end
 					  csv << merged
-            line_count +=1
-				  rescue StopIteration
-            done = true
-					  break
-				  end
+		        line_count +=1
+		      end  
+			  rescue StopIteration
+          done = true
+				  break
 			  end
+
+			end
         file_index += 1
-		  end
     end
+	end
+
+	def sort(file)
+		output 					 = "#{file}.sorted"
+		content_as_table = parse(file)
+		headers 				 = content_as_table.headers
+		index_of_key 		 = headers.index('Clicks')
+		content 				 = content_as_table.sort_by { |a| -a[index_of_key].to_i }
+		write(content, headers, output)
+		return output
 	end
 
 	private
@@ -90,10 +114,10 @@ class Modifier
 		FLOAT_VALUES.each do |key|
 			hash[key] = hash[key][0].from_german_to_f.to_german_s
 		end
-		['number of commissions'].each do |key|
+		NUM_OF_COMMISSIONS.each do |key|
 			hash[key] = (@cancellation_factor * hash[key][0].from_german_to_f).to_german_s
 		end
-		['Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value'].each do |key|
+		COMMISSION_VALUES.each do |key|
 			hash[key] = (@cancellation_factor * @saleamount_factor * hash[key][0].from_german_to_f).to_german_s
 		end
 		hash
@@ -101,36 +125,31 @@ class Modifier
 
 	def combine_hashes(list_of_rows)
 		keys = []
-		list_of_rows.each do |row|
-			next if row.nil?
-			row.headers.each do |key|
-				keys << key
-			end
-		end
+		keys = list_of_rows.compact.map(&:headers)
+		keys.flatten!
+
 		result = {}
 		keys.each do |key|
 			result[key] = []
-			list_of_rows.each do |row|
-				result[key] << (row.nil? ? nil : row[key])
-			end
+			result[key] = list_of_rows.map { |r| r.nil? ? nil : r[key]  }
 		end
 		result
 	end
 
 	def parse(file)
-		CSV.read(file, DEFAULT_CSV_OPTIONS)
+		CSV.read(file, READ_CSV_OPTIONS)
 	end
 
 	def lazy_read(file)
 		Enumerator.new do |yielder|
-			CSV.foreach(file, DEFAULT_CSV_OPTIONS) do |row|
+			CSV.foreach(file, READ_CSV_OPTIONS) do |row|
 				yielder.yield(row)
 			end
 		end
 	end
 
 	def write(content, headers, output)
-		CSV.open(output, "wb", { col_sep: "\t", headers: :first_row, row_sep: "\r\n" }) do |csv|
+		CSV.open(output, "wb", WRITE_CSV_OPTIONS) do |csv|
 			csv << headers
 			content.each do |row|
 				csv << row
@@ -138,14 +157,4 @@ class Modifier
 		end
 	end
 
-	public
-	def sort(file)
-		output = "#{file}.sorted"
-		content_as_table = parse(file)
-		headers = content_as_table.headers
-		index_of_key = headers.index('Clicks')
-		content = content_as_table.sort_by { |a| -a[index_of_key].to_i }
-		write(content, headers, output)
-		return output
-	end
 end
