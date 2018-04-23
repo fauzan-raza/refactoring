7697535f08c15241a9f630a7299d3f95d488d35a
Fauzan Raza <fauzan.raza@keeptruckin.com>
2018-04-23 13:08:19 +0500

    test cases to be written for methods in modifier.rb

--- spec/modifier_spec.rb
+++ spec/modifier_spec.rb
@@ -2,5 +2,35 @@ require File.expand_path('spec_helper', File.dirname(__FILE__))
 require 'modifier'
 
 describe Modifier do
-  
+  context "#sort" do
+
+  end
+
+  context "#combine" do
+    
+  end
+
+  context "#modify" do
+    
+  end
+
+  context "#create_files" do
+    
+  end
+
+  context "#lazy_read" do
+    
+  end
+
+  context "#write" do
+    
+  end
+
+  context "#combine_values" do
+    
+  end
+
+  context "#combine_hashes" do
+    
+  end
 end
